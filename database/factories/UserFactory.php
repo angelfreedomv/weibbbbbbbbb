a8f700da1a1b213487da67fe96b23a8c474cbbf6
angelfreedomv <echopan1234@gmail.com>
2019-05-04 09:56:25 +0000

    粉丝数据表

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -9,7 +9,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'activated' => true,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => '$2y$10$mti511jscmk9bV6hkjrX0uhrP5sHr42LsfcCwb8a8Ioiqm60/r8h.', // secret
         'remember_token' => str_random(10),
         'created_at' => $date_time,
         'updated_at' => $date_time,
